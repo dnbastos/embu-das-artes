5bff8d63d8f3c36c249fc73cb9dfb501038c21e8
Daniel Bastos <danielbastos.17@hotmail.com>
2019-07-18 16:37:16 -0300

    Remove react-geocode library

--- src/PlacesAPI.js
+++ src/PlacesAPI.js
@@ -1,19 +1,24 @@
-import Geocode from 'react-geocode';
+// import Geocode from 'react-geocode';
 import myPlaces from './myPlaces.json';
 
+const api = 'https://maps.google.com/maps/api/geocode/json';
 const apiKey = 'AIzaSyC0LJxzJG83wmuruULMypSFxo6nypBS_bY';
-Geocode.setApiKey(apiKey);
+//Geocode.setApiKey(apiKey);
 
 const getMyPlaceById = id => myPlaces.places.filter((place, i) => i === id)[0];
 
-const getQuery = placeId => {
+const getPlaceAddress = placeId => {
  const myPlace = getMyPlaceById(placeId);
  return `${myPlace.name} ${myPlaces.neighborhood}, ${myPlaces.city} - ${myPlaces.state}`;
 }
 
+const getUrlQuery = (address) => `address=${address}&key=${apiKey}`;
+
 export const get = placeId => {
   const myPlace = myPlaces.places[placeId];
-  return Geocode.fromAddress(getQuery(placeId))
+  const placeAddress = getPlaceAddress(placeId);
+  return fetch(`${api}?${getUrlQuery(placeAddress)}`)//.fromAddress(getPlaceAddress(placeId))
+    .then(response => response.json())
     .then(data => data.results[0])
     .then(data => {
       const places = {...data, ...myPlace};
